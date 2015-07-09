9a85595d4db77dfe8f69d87669396731c56ee88f
Frank <fylin134@gmail.com>
2015-07-09 13:18:52 -0500

    Started on saving journal data to csv files

--- app/src/main/java/com/example/frank_eltank/macro_meter/JournalReader.java
+++ app/src/main/java/com/example/frank_eltank/macro_meter/JournalReader.java
@@ -4,6 +4,14 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.List;
+
 /**
  * This class is used by JournalActivity to read saved journal entries per day that
  * are saved as Comma Separated Values (CSV) files.
@@ -12,11 +20,12 @@ import android.preference.PreferenceManager;
  *
  * CSV Representation Schema:
  *
- * DATE1, FOOD1, CALORIES1, QUANTITY1, FAT1, CARBS1, PROTEIN1,
+ * FOOD1, CALORIES1, QUANTITY1, FAT1, CARBS1, PROTEIN1,
  * FOOD2, CALORIES2, etc...
  * ...
  * ...
- * DATE2, FOOD1, CALORIES1
+ *
+ * Each CSV file is saved as YEARMONTHDAY eg: 20150708
  *
  *
  * Created by Frank on 7/8/2015.
@@ -26,6 +35,7 @@ public class JournalReader {
     private Context mContext;
     private SharedPreferences mSettings;
     private SharedPreferences.Editor mEditor;
+    private Calendar mCalendar;
 
     public JournalReader(Context context){
         mContext = context;
@@ -34,5 +44,40 @@ public class JournalReader {
     public void readCurrentJournal(){
         mSettings = PreferenceManager.getDefaultSharedPreferences(mContext);
         mEditor = mSettings.edit();
+
+        mCalendar = Calendar.getInstance();
+        String journalFileName = ""+mCalendar.get(Calendar.YEAR) +""+mCalendar.get(Calendar.MONTH)+""+mCalendar.get(Calendar.DATE);
+
+        File journal = new File(mContext.getFilesDir(), journalFileName);
+        // Check if the current date journal exists
+        if(journal.exists()){
+            try {
+                FileReader reader = new FileReader(journal);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        // Create an empty journal file
+        else{
+            try {
+                journal.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void writeJournal(List<String> data, String journalFileName){
+        File journal = new File(mContext.getFilesDir(), journalFileName);
+        try {
+            FileWriter writer = new FileWriter(journal);
+            for(String s : data){
+                writer.write(s);
+            }
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
